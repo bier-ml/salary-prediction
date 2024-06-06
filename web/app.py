36b37e3c0077beaf2e54038ab826bf9985e7bd4c
Artem <artichek123@gmail.com>
2024-06-06 22:48:17 +0300

    Add regexp entities

--- web/app.py
+++ web/app.py
@@ -5,21 +5,91 @@ from core.embedding_models import EmbeddingModel, FastTextEmbeddingModel
 from core.models.clustering_model import StackedModels, ClusteringModel
 from web import ROOT_PATH
 
+import re
+
 
 class MockNERModel:
     def __init__(self):
-        self.entities = {
-            "job_name": ["Software Engineer", "Data Scientist"],
-            "schedule": ["Full-time", "Part-time"],
-            "city": ["San Francisco", "New York"],
-        }
+        self.entities = {}
+
+    def extract_entities(self, text):
+        text = text.lower()  # преобразуем текст в нижний регистр
+        entities = {}
+
+        # Пол
+        match = re.search(r'лет, (.+?)[\n\r]', text)
+        if match:
+            entities['Пол'] = match.group(1).strip().replace('\n', '').replace('.', '').replace(',', '')
+        else:
+            entities['Пол'] = ''
+
+        # Желаемая должность
+        match = re.search(r'\nзарплата (.+?)[\n\r]', text)
+        if match:
+            entities['Желаемая должность'] = match.group(1).strip().replace('\n', '').replace('.', '').replace(',', '')
+        else:
+            entities['Желаемая должность'] = ''
+
+        # График работы
+        match = re.search(r'график, место работы (.+?)ищу работу в городе:', text)
+        if match:
+            entities['График работы'] = match.group(1).strip().replace('\n', '').replace('.', '').replace(',', '')
+        else:
+            entities['График работы'] = ''
+
+        # Место работы
+        match = re.search(r'ищу работу в городе: (.+?)\.', text)
+        if match:
+            entities['Место работы'] = match.group(1).strip().replace('\n', '').replace('.', '').replace(',', '')
+        else:
+            entities['Место работы'] = ''
+
+        # Стаж работы
+        match = re.search(r'стаж в желаемой должности (.+?)[\n\r]', text)
+        if match:
+            entities['Стаж работы'] = match.group(1).strip().replace('\n', '').replace('.', '').replace(',', '')
+        else:
+            entities['Стаж работы'] = ''
+
+        # Образование
+        match = re.search(r'основное образование (.+?),', text)
+        if match:
+            entities['Образование'] = match.group(1).strip().replace('\n', '').replace('.', '').replace(',', '')
+        else:
+            entities['Образование'] = ''
+
+        # Навыки
+        match = re.search(r'профессиональные навыки (.+?)(?:основное образование|опыт работы)', text, re.DOTALL)
+        if match:
+            entities['Навыки'] = match.group(1).strip().replace('\n', '').replace('.', '').replace(',', '')
+        else:
+            entities['Навыки'] = ''
+
+        # объединяем все значения из словаря entities путем их конкатенации
+        result_str = ' '.join(entities.values())
+        result_str = result_str.replace("\n", " ")
+
+        return entities, result_str
 
     def predict(self, text):
-        extracted_entities = {"job_name": [], "schedule": [], "city": []}
+        extracted_entities = {}
+
+        # Use predefined entities to extract data
         for entity, examples in self.entities.items():
             for example in examples:
-                if example in text:
+                if example.lower() in text:
                     extracted_entities[entity].append(example)
+
+        # Extract additional entities using the extract_entities method
+        additional_entities, result_str = self.extract_entities(text)
+
+        # Merge the extracted entities into the result
+        for key in additional_entities:
+            if key not in extracted_entities:
+                extracted_entities[key] = []
+            if additional_entities[key]:
+                extracted_entities[key].append(additional_entities[key])
+
         return extracted_entities
 
 
@@ -46,7 +116,7 @@ embedding_mapping = {
 
 
 def load_model(
-    name: str = "stacked_model_fasttext",
+        name: str = "stacked_model_fasttext",
 ) -> tuple[StackedModels, EmbeddingModel | FastTextEmbeddingModel]:
     models_mapping = {
         "stacked_model_fasttext": (
@@ -96,8 +166,6 @@ def run_server():
     run_button = st.button("Запустить")
     result_placeholder = st.empty()
 
-    model = "test"
-    source = "test"
     if run_button:
         if pdf_file:  # job_name and model and schedule and city:
             pdf_to_text = PDFToText(pdf_file)
@@ -115,7 +183,7 @@ def run_server():
             result_placeholder.text("Processing...")
             st.subheader("Predicted Salary")
 
-            source_embedding = embedding_model.generate(source)
+            source_embedding = embedding_model.generate(extracted_text)
             result = pretrained_model.predict(source_embedding)
 
             result_placeholder.text("0")
